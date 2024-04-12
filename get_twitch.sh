30d65fefa3c91540b982a9e92e1d771df3db4cce
Tingting Tang <tangting@usc.edu>
2024-04-11 17:26:34 -0700

    verified get_twitch script

--- get_twitch.sh
+++ get_twitch.sh
@@ -1,6 +1,9 @@
 wget https://snap.stanford.edu/data/twitch.zip
 unzip twitch.zip -d data/
+rm twitch.zip
 cd data
 mv twitch linkteller-data
-cd DE
+cd linkteller-data/DE
 mv musae_DE.json musae_DE_features.json
+cd ..
+rm *.txt
