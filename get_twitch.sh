c0b9f8a8e72f00ad9ea800026606f24bdc127316
Tingting Tang <tangting@usc.edu>
2024-04-15 18:24:06 -0700

    added twitch dataset in data directory

--- get_twitch.sh
+++
@@ -1,9 +0,0 @@
-wget https://snap.stanford.edu/data/twitch.zip
-unzip twitch.zip -d data/
-rm twitch.zip
-cd data
-mv twitch linkteller-data
-cd linkteller-data/DE
-mv musae_DE.json musae_DE_features.json
-cd ..
-rm *.txt
