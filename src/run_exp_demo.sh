5fc4f2d885e8d2f6a79f8979c01ebdee7d95e708
Tingting Tang <tangting@usc.edu>
2024-05-23 00:09:08 -0700

    updated run_exp_demo shell script, spacing in if elif condition

--- src/run_exp_demo.sh
+++ src/run_exp_demo.sh
@@ -1,8 +1,8 @@
-python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --only_create_todos --datasets $1
-python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --datasets $1
-python parser_ash.py --results_dir ../results
-python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --only_create_todos --datasets $1
-python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --datasets $1
+# python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --only_create_todos --datasets $1
+# python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --datasets $1
+# python parser_ash.py --results_dir ../results
+# python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --only_create_todos --datasets $1
+# python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --datasets $1
 
 array=()
 IFS=',' 
@@ -16,31 +16,31 @@ do
     then
         echo "$dataset"
         python parser_ash_trans_attack.py --results_dir ./eval_citeseer
-    elif ["$dataset" == "PubMed"]
+    elif [ "$dataset" == "PubMed" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_pubmed
-    elif ["$dataset" == "facebook_page"] 
+    elif [ "$dataset" == "facebook_page" ] 
     then
         python parser_ash_trans_attack.py --results_dir ./eval_facebook_page
-    elif ["$dataset" == "Chameleon"]
+    elif [ "$dataset" == "Chameleon" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_chameleon
-    elif ["$dataset" == "TwitchES"]
+    elif [ "$dataset" == "TwitchES" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/ES
-    elif ["$dataset" == "TwitchRU"]
+    elif [ "$dataset" == "TwitchRU" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/RU
-    elif ["$dataset" == "TwitchDE"]
+    elif [ "$dataset" == "TwitchDE" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/DE
-    elif ["$dataset" == "TwitchFR"]
+    elif [ "$dataset" == "TwitchFR" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/FR
-    elif ["$dataset" == "TwitchENGB"]
+    elif [ "$dataset" == "TwitchENGB" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/ENGB
-    elif ["$dataset" == "TwitchPTBR"]
+    elif [ "$dataset" == "TwitchPTBR" ]
     then
         python parser_ash_trans_attack.py --results_dir ./eval_twitch/PTBR
     fi
