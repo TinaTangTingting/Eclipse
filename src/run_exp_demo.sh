1832c7e7db605810887eb5b65ba484d2f23bcf35
Tingting Tang <tangting@usc.edu>
2024-05-23 21:26:59 -0700

    enable the commented out lines in run_exp_demo script

--- src/run_exp_demo.sh
+++ src/run_exp_demo.sh
@@ -1,8 +1,8 @@
-# python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --only_create_todos --datasets $1
-# python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --datasets $1
-# python parser_ash.py --results_dir ../results
-# python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --only_create_todos --datasets $1
-# python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --datasets $1
+python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --only_create_todos --datasets $1
+python run_exp_demo.py --rank 20  --num_seeds 5 --command train --outdir ../results --todos_dir ../todos_train --best_config_file best_config.pkl --datasets $1
+python parser_ash.py --results_dir ../results
+python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --only_create_todos --datasets $1
+python run_exp_demo.py --rank 20  --num_seeds 5 --command attack --outdir ../results --todos_dir ../todos_attack --best_config_file best_config.pkl --datasets $1
 
 array=()
 IFS=',' 
